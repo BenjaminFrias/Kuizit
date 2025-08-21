89b1bfcc1c7365a4cef00a028b3ab69ea384e587
benjaminfrias <benjaminfrias2004@gmail.com>
2025-08-21 16:05:44 -0400

    feat(server): handle quiz generation from youtube links

---
+++ server/src/services/generateQuiz.ts
@@ -0,0 +1,208 @@
+import {
+	GoogleGenAI,
+	createPartFromUri,
+	createUserContent,
+	HarmBlockThreshold,
+	HarmCategory,
+	Type,
+	Content,
+	Part,
+	FinishReason,
+} from '@google/genai';
+
+const API_KEY = process.env.GEMINI_API_KEY;
+
+const ai = new GoogleGenAI({ apiKey: API_KEY });
+
+export async function generateQuiz(
+	quizInputType: string,
+	quizContent: string | Express.Multer.File,
+	numQuestions: number,
+	difficulty: string,
+	optionTypes: string[]
+) {
+	const safetySettings = [
+		{
+			category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+			threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+		},
+		{
+			category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+			threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+		},
+		{
+			category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+			threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+		},
+		{
+			category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+			threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+		},
+	];
+
+	const configAI = {
+		systemInstruction: `
+        You are an expert quiz question generator. Your task is to create quiz questions based on provided content.
+        Adhere strictly to the following rules:
+        - Generate exactly ${numQuestions} questions.
+        - The difficulty level for all questions must be ${difficulty}.
+        - Questions should be concise, ideally under 20 words.
+        - Options should be brief and clear.
+        - Randomly mix question types from the provided array:${optionTypes}.
+        - For 'multiple_choice' questions:
+            - Provide exactly 4 options.
+            - Ensure only ONE option has 'answer: true'. The other three must be plausible distractors with 'answer: false'.
+        - For 'true_false' questions:
+            - Provide exactly 2 options: one for 'True' and one for 'False'.
+            - Ensure only ONE option has 'answer: true'.
+        - The response MUST strictly follow the provided JSON schema.
+        - You should provide a longer explanation of the answer, ideally between 20 and 50 words.
+		- If the content does not met the safety settings return an empty array
+        `,
+		safetySettings: safetySettings,
+		responseMimeType: 'application/json',
+		responseSchema: {
+			type: Type.ARRAY,
+			items: {
+				type: Type.OBJECT,
+				properties: {
+					type: {
+						type: Type.STRING,
+						enum: optionTypes,
+					},
+					question: {
+						type: Type.STRING,
+					},
+					correctAnswerIndex: {
+						type: Type.NUMBER,
+					},
+					options: {
+						type: Type.ARRAY,
+						items: {
+							type: Type.OBJECT,
+							properties: {
+								optionText: {
+									type: Type.STRING,
+								},
+								answer: {
+									type: Type.BOOLEAN,
+								},
+							},
+						},
+					},
+					explanation: {
+						type: Type.STRING,
+					},
+				},
+				propertyOrdering: [
+					'type',
+					'correctAnswerIndex',
+					'question',
+					'options',
+					'explanation',
+				],
+			},
+		},
+	};
+
+	try {
+		// Get correct content for AI based on quiz type (file or prompt)
+		let validQuizContent:
+			| string
+			| (string | { fileData: { fileUri: string } })[]
+			| Content = '';
+
+		if (quizInputType === 'prompt' && typeof quizContent === 'string') {
+			validQuizContent = quizContent;
+		} else if (quizInputType === 'file' && typeof quizContent !== 'string') {
+			validQuizContent = await getContentFromFile(quizContent);
+		} else if (
+			quizInputType === 'youtube_link' &&
+			typeof quizContent === 'string'
+		) {
+			validQuizContent = [
+				'Please create a quiz based on the content on this video.',
+				{
+					fileData: {
+						fileUri: quizContent,
+					},
+				},
+			];
+		}
+
+		const response = await ai.models.generateContent({
+			model: 'gemini-2.5-flash',
+			contents: validQuizContent,
+			config: configAI,
+		});
+
+		if (response.promptFeedback?.blockReason) {
+			throw new Error(
+				`Content generation failed: Prompt blocked (reason: ${response.promptFeedback.blockReason})`
+			);
+		}
+
+		// Check for response blockage
+		if (!response.candidates || response.candidates.length === 0) {
+			throw new Error('Content generation failed: No candidates returned.');
+		}
+
+		const firstCandidate = response.candidates[0];
+
+		// Check for finish reasons other than STOP
+		if (
+			firstCandidate.finishReason &&
+			firstCandidate.finishReason !== FinishReason.STOP
+		) {
+			if (firstCandidate.finishReason === FinishReason.SAFETY) {
+				throw new Error(
+					'Content generation failed: Response blocked due to safety settings.'
+				);
+			} else {
+				throw new Error(
+					`Content generation failed: Stopped due to ${firstCandidate.finishReason}.`
+				);
+			}
+		}
+
+		const responseText = response?.candidates?.[0]?.content?.parts?.[0]?.text;
+
+		if (typeof responseText !== 'string' || responseText.length === 0) {
+			throw new Error('AI returned no text content.');
+		}
+
+		const quizData = JSON.parse(responseText);
+
+		if (Array.isArray(quizData) && quizData.length > 0) {
+			return quizData;
+		} else {
+			throw new Error('Parsed data is empty due to invalid content.');
+		}
+	} catch (error: any) {
+		console.error('Failed to generate quiz: ', error);
+		throw new Error('Could not generate quiz. Please try again.');
+	}
+}
+
+async function getContentFromFile(file: Express.Multer.File) {
+	const { buffer, mimetype } = file;
+
+	try {
+		const fileBlob = new Blob([buffer], { type: mimetype });
+
+		// Pass the Blob object to the upload method
+		const uploadedFile = await ai.files.upload({
+			file: fileBlob,
+		});
+
+		return createUserContent([
+			createPartFromUri(
+				uploadedFile.uri ? uploadedFile.uri : '',
+				uploadedFile.mimeType ? uploadedFile.mimeType : ''
+			),
+		]);
+	} catch (error) {
+		console.error('Error processing file with Gemini:', error);
+		throw error;
+	}
+}
