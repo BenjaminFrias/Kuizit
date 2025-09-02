3f967a1c8e5280290616f38bf1772280b5b7e351
benjaminfrias <benjaminfrias2004@gmail.com>
2025-09-02 18:03:41 -0400

    remove predesign and add blurry shape component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,27 @@
-import { useState } from 'react';
 import './App.css';
+import BlurryShape from './components/blurryShape';
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
-			<div></div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
+			<div className="min-h-screen min-w-screen p-8 flex flex-col items-center justify-center bg-custom-gray overflow-hidden">
+				<div className="flex flex-col items-center justify-center z-3">
+					<h2 className=" text-white text-2xl">Quizify</h2>
+					<h1 className="font-primary font-medium text-white text-center w-full text-8xl max-w-200">
+						Instantly create quizzes with AI
+					</h1>
+					<p className="font-primary text-white text-lg text-center max-w-120 mt-8">
+						Turn any text, YouTube video, or content into an interactive quiz in
+						seconds!
+					</p>
+				</div>
+				<div className="absolute -bottom-0 -top-10 -right-100 w-300 h-300">
+					<BlurryShape />
+				</div>
+				<div className="absolute -top-100 -left-90 w-200 h-200 opacity-70">
+					<BlurryShape />
+				</div>
 			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
 		</>
 	);
 }
---
+++ client/src/components/blurryShape.tsx
@@ -0,0 +1,15 @@
+export default function BlurryShape() {
+	return (
+		<div
+			className="w-full h-full rounded-full bg-gradient-to-br blur-sm opacity-80"
+			style={{
+				filter: 'blur(60px) contrast(1)',
+				background: `
+              radial-gradient(circle at 30% 30%, rgba(57,114,54, 1), transparent 50%),
+              radial-gradient(circle at 70% 70%, rgba(66,102,65, 1), transparent 50%),
+              linear-gradient(135deg, #289722, #3d9238),
+              url("data:image/svg+xml,%3Csvg viewBox='0 0 256 256' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.15'/%3E%3C/svg%3E")`,
+			}}
+		></div>
+	);
+}
