f78737464867f43bdf1ad88aa131313493c096cb
benjaminfrias <benjaminfrias2004@gmail.com>
2025-09-06 10:03:26 -0400

    style: create magic text illustration

---
+++ client/src/components/decorative/MagicTextIllustration.tsx
@@ -0,0 +1,20 @@
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function MagicTextIllustration() {
+	return (
+		<div className="w-55 h-70 flex flex-col gap-8 p-8 bg-black/70 rounded-4xl drop-shadow-[13px_11px_8px_rgba(0,0,0,1)] inset-shadow-md inset-shadow-primary/50">
+			<div className="flex gap-4 justify-center items-center">
+				<FontAwesomeIcon className="text-[#caebcc]/80 text-4xl" icon={faStar} />
+				<div className="w-full h-5 bg-primary rounded-full"></div>
+			</div>
+			<div>
+				<div className="w-full h-5 bg-primary rounded-full"></div>
+			</div>
+			<div className="flex gap-4 justify-center items-center">
+				<div className="w-full h-5 bg-primary rounded-full"></div>
+				<FontAwesomeIcon className="text-[#caebcc]/80 text-4xl" icon={faStar} />
+			</div>
+		</div>
+	);
+}
--- client/src/components/decorative/OptionsCheckIllustration.tsx
+++ client/src/components/decorative/OptionsCheckIllustration.tsx
@@ -3,8 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function OptionsCheckIllustration() {
 	return (
-		<div className="w-55 h-70 flex flex-col justify-between gap-2 p-7 bg-black/50 rounded-4xl drop-shadow-[13px_11px_8px_rgba(0,0,0,1)]">
-			<div className="w-full flex justify-center items-center flex-1 bg-[#569D5B] rounded-full">
+		<div className="w-55 h-70 flex flex-col justify-between gap-2 p-7 bg-black/60 rounded-4xl drop-shadow-[13px_11px_8px_rgba(0,0,0,1)] inset-shadow-md inset-shadow-primary/15">
+			<div className="w-full flex justify-center items-center flex-1 bg-primary rounded-full">
 				<FontAwesomeIcon className="text-[#caebcc] text-2xl" icon={faCheck} />
 			</div>
 			<div className="w-full flex justify-center items-center flex-1 bg-white/10 rounded-full"></div>
