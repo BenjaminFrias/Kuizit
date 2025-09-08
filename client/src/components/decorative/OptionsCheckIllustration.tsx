4e5f1347a680293506f978bb46ee583bb45f653c
benjaminfrias <benjaminfrias2004@gmail.com>
2025-09-08 17:32:21 -0400

    style: add responsive design to homepage

--- client/src/components/decorative/OptionsCheckIllustration.tsx
+++ client/src/components/decorative/OptionsCheckIllustration.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function OptionsCheckIllustration() {
 	return (
-		<div className="w-55 h-70 flex flex-col justify-between gap-2 p-7 bg-black/60 rounded-4xl drop-shadow-[13px_11px_8px_rgba(0,0,0,1)] inset-shadow-md inset-shadow-primary/15">
+		<div className="w-full h-full flex flex-col justify-between gap-2 p-7 bg-black/60 rounded-4xl drop-shadow-[13px_11px_8px_rgba(0,0,0,1)] inset-shadow-md inset-shadow-primary/15">
 			<div className="w-full flex justify-center items-center flex-1 bg-primary rounded-full">
 				<FontAwesomeIcon className="text-[#caebcc] text-2xl" icon={faCheck} />
 			</div>
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -4,55 +4,73 @@ import OptionsCheckIllustration from '@/components/decorative/OptionsCheckIllust
 import { Button } from '@/components/ui/button';
 import { faWandMagicSparkles } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import type { onPageChangeType } from '@/types';
 
-export default function HomePage() {
+export default function HomePage({ onPageChange }: onPageChangeType) {
 	return (
-		<div className="min-h-screen min-w-screen p-8 flex flex-col items-center justify-center overflow-hidden bg-custom-gray">
+		<div className="min-h-screen min-w-screen p-6 md:p-8 flex flex-col items-center justify-center overflow-hidden bg-custom-gray">
 			<div className="absolute flex flex-col items-center w-full h-full z-999">
-				<h2 className="absolute top-10 center font-logo text-white text-lg uppercase">
+				<h2 className="absolute z-999 top-10 center font-logo text-white text-lg uppercase">
 					Kuizit
 				</h2>
 
-				<div className="flex flex-col items-center justify-center flex-grow mt-14">
+				<div className="flex flex-col items-center justify-center flex-grow px-8 md:mt-14">
 					<h1
 						className="
-						font-primary font-medium text-white text-shadow-title text-center w-full text-[6.5rem] tracking-wide max-w-200"
+						font-primary font-medium text-white text-shadow-title text-center w-full text-7xl md:text-[6.5rem] tracking-wide max-w-200"
 					>
 						Instantly create quizzes with AI
 					</h1>
 
-					<p className="font-primary text-white text-lg text-center max-w-120 mt-8">
+					<p className="font-primary text-white text-lg text-center md:max-w-120 mt-8 z-999 md:z-1">
 						Turn any text, YouTube video, or content into an interactive quiz in
 						seconds!
 					</p>
-					<Button className="mt-12" size="lg" variant="green">
+					<Button
+						className="mt-12 z-999"
+						size="lg"
+						variant="green"
+						onClick={() => onPageChange('input')}
+					>
 						Generate quiz
 						<FontAwesomeIcon icon={faWandMagicSparkles} />
 					</Button>
 
-					<div className="absolute -bottom-20 left-30 -rotate-12">
+					<div
+						className="absolute -bottom-30 left-10 w-35 h-60 lg:w-65 lg:h-80 -rotate-25
+					md:w-55 md:h-70 md: md:-left-5 md:-rotate-12 lg:left-30 z-4"
+					>
 						<OptionsCheckIllustration />
 					</div>
 
-					<div className="absolute -bottom-20 right-30 rotate-12 z-2">
+					<div
+						className="absolute -bottom-30 right-10 w-35 h-60 rotate-25
+					md:w-55 md:h-70 md:right-2 lg:right-30 lg:w-65 lg:h-80 md:rotate-12 z-4"
+					>
 						<MagicTextIllustration />
 					</div>
 
-					{/* Blurry shapes for blurry effect on h1 */}
-					<div className="absolute -top-100 -left-110 w-200 h-200 opacity-80">
+					{/* Blurry shapes for blurry opacity effect on h1 */}
+					<div
+						className="absolute -top-50 w-90 h-90 -left-25 md:w-140
+						md:h-140 md:-top-50 md:-left-70 lg:-top-100 lg:-left-110 lg:w-200 lg:h-200 opacity-80 z-3"
+					>
 						<BlurryShape />
 					</div>
 
-					<div className="absolute -bottom-0 -top-10 -right-190 w-300 h-300">
+					<div
+						className="absolute -bottom-45 -right-12 w-120 h-120 md:w-240 md:h-240 md:-bottom-0
+						md:-top-10 md:-right-190 lg:w-300 lg:h-300 z-3"
+					>
 						<BlurryShape />
 					</div>
 				</div>
 			</div>
 			<div className="z-1">
-				<div className="absolute -bottom-0 -top-10 -right-100 w-300 h-300 -z-2">
+				<div className="absolute -bottom-25 -right-32 w-120 h-120 md:-top-10 md:-right-100 md:w-300 md:h-300 -z-2">
 					<BlurryShape />
 				</div>
-				<div className="absolute -top-100 -left-90 w-200 h-200 opacity-80 -z-2">
+				<div className="absolute -top-55 w-90 h-90 -left-25 md:-top-100 md:-left-90 md:w-200 md:h-200 opacity-80 -z-2">
 					<BlurryShape />
 				</div>
 			</div>
