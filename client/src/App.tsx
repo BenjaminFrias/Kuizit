204aa7a353eaa645b6513d2dbc1c55b6286c6a0a
benjaminfrias <benjaminfrias2004@gmail.com>
2025-09-08 17:33:30 -0400

    feat: add current page state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import HomePage from './pages/HomePage';
+import Inputpage from './pages/InputPage';
 
 function App() {
-	return <HomePage />;
+	const [currentPage, setcurrentPage] = useState('home');
+
+	const handlePageChange = (pageName: string) => {
+		setcurrentPage(pageName);
+	};
+
+	switch (currentPage) {
+		case 'home':
+			return <HomePage onPageChange={handlePageChange} />;
+		case 'input':
+			return <Inputpage onPageChange={handlePageChange} />;
+		default:
+			return <HomePage onPageChange={handlePageChange} />;
+	}
 }
 
 export default App;
