fecae3c2febce3627016b6ed930f9e0d465c0625
benjaminfrias <benjaminfrias2004@gmail.com>
2025-09-24 12:25:39 -0400

    style(loading): add fa pulse class to loading animation icon

--- client/src/pages/QuizLoadingPage.tsx
+++ client/src/pages/QuizLoadingPage.tsx
@@ -4,11 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const LoadingAnimation = () => {
 	const t = useTranslation();
+
 	return (
 		<div className="flex flex-col min-h-screen min-w-screen bg-custom-white items-center justify-center">
 			<FontAwesomeIcon
 				icon={faSpinner}
-				className="animate-spin text-3xl text-custom-green"
+				className="animate-spin fa-pulse text-3xl text-custom-green"
 			/>
 			<p className="mt-4 text-custom-light-gray text-2xl font-medium font-primary">
 				{t.generating}...
